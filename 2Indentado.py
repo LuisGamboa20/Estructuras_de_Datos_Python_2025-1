e840d94bc4c97bb5d8f7de18ef37906bfbd12ac9
LuisGamboa20 <luiscarlosjunior2016@hotmail.com>
2025-02-13 15:30:34 -0500

    Se ha añadido un nuevo archivo

---
+++ 2Indentado.py
@@ -0,0 +1,2 @@
+if 5 > 2:
+    print("Cinco es mayor que dos")
---
+++ 3Variables.py
@@ -0,0 +1,23 @@
+# Esta es la forma correcta de declarar variables en Python
+x = 5
+y = "Hola Mundo"
+
+print(y)
+print(x)
+print(x,y)
+"""
+Comentarios de varias líneas
+"""
+
+# Casting de variables 
+
+x = str(3)   
+y = int(3)
+z = float(3)
+#a = 13.3*x
+b = int(13.3)*y
+c = 13.3*z
+
+print(b,c)
+print(type(x), "La variable x es de este tipo")
+
